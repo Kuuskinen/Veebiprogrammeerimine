6d63dfe6d06212b8dd891b71661e2b3a3988ada1
Kuuskinen <Kuuskinen@users.noreply.github.com>
2017-09-14 10:47:06 +0300

    Lisatud on esimene PHP koodijupp

---
+++ esimene.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Meelis Lutsar</title>
+</head>
+<body>
+   <h1>Meelis Lutsar, veebiprogrammeerimine</h1>
+   <p>See veebileht on loodud õppetöö raames ja ei sisalda mingit tõsiselt võetavat sisu.<p>
+See tekst on lisatud koduse töö raames, pärast korduvaid katseid selleni jõuda. Ma loodan, et see tekst püsib siin 
+neljapäevani.<br>
+
+<p>Pluuto avastas 1930. aastal  Clyde Tombaugh  ja alates avastamisest kuni 2006. aastani nimetati teda planeediks<br> 
+ning loeti Päikesesüsteemi üheksandaks planeediks. Pluuto planeedistaatus seati kahtluse alla<br>
+1992. aastal, kui Kuiperi vööst leiti mitu Pluutoga sarnast objekti. 2005. aastal avastati Kuiperi vööst<br>
+kääbusplaneet  Eris, mis on Pluutost 27% suurem. 24. augustil  2006 otsustas  Rahvusvaheline<br>
+Astronoomiaunioon  võtta Pluutolt planeedi staatuse ja kvalifitseeris Pluuto ümber kääbusplaneediks.<p>
+<?php
+echo "Algas PHP õppimine";
+?>
+</body>
+</html>
